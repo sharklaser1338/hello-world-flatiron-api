9113b68b53e75542e95304354d5daea5766f95a6
sharklaser1338 <d7qtcvp+ybzjws@sharklasers.com>
2013-08-07 11:29:51 +0200

    asdf

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ my_http = require("http");
 my_http.createServer(function(request,response){  
     sys.puts("I got kicked");  
     response.writeHeader(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World");  
+    response.write("Hallo Hannes, Hallo Ben");  
     response.end();  
 }).listen(8080);  
 sys.puts("Server Running on 8080");
