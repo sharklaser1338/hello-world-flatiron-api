1f3d0f315b15e921c05b9e123f8c23a8f5c3972d
sharklaser1338 <d7qtcvp+ybzjws@sharklasers.com>
2013-08-07 11:51:13 +0200

    fasd

--- index.js
+++ index.js
@@ -9,64 +9,22 @@ app.use(flatiron.plugins.http,
   }
 });
 
-var port = 8080;
-var serverUrl = "sharklaser1338.jit.su";
+var sys = require("sys"),  
+my_http = require("http"),
+fs = require('fs'),
+
+filepath = "index.html";
  
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
- 
-console.log("Starting web server at " + serverUrl + ":" + port);
- 
-http.createServer( function(req, res) {
- 
-var now = new Date();
- 
-var filename = req.url || "index.html";
-var ext = path.extname(filename);
-var localPath = __dirname;
-var validExtensions = {
-".html" : "text/html",	
-".js": "application/javascript",
-".css": "text/css",
-".txt": "text/plain",
-".jpg": "image/jpeg",
-".gif": "image/gif",
-".png": "image/png"
-};
-var isValidExt = validExtensions[ext];
- 
-if (isValidExt) {
-localPath += filename;
-path.exists(localPath, function(exists) {
-if(exists) {
-console.log("Serving file: " + localPath);
-getFile(localPath, res, ext);
-} else {
-console.log("File not found: " + localPath);
-res.writeHead(404);
-res.end();
-}
-});
- 
-} else {
-console.log("Invalid file extension detected: " + ext)
-}
- 
-}).listen(port, serverUrl);
- 
-function getFile(localPath, res, mimeType) {
-fs.readFile(localPath, function(err, contents) {
-if(!err) {
-res.setHeader("Content-Length", contents.length);
-res.setHeader("Content-Type", mimeType);
-res.statusCode = 200;
-res.end(contents);
-} else {
-res.writeHead(500);
-res.end();
-}
-});
-}
+ fs.readFile(filepath, function(err, html){
+ 	my_http.createServer(function(request,response){  
+    	sys.puts("I got kicked");  
+    	response.writeHeader(200, {"Content-Type": "text/plain"});  
+    	response.write(html);  
+    	response.end();  
+		}).listen(8080);  
+		sys.puts("Server Running on 8080");
+ });
+
+
 
 app.start(3000, function () { console.log({"flatiron": "ok"}); });
